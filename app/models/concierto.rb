guillermobp/edmchc-20190412

:bug: formatea fechas y horas de conciertos ssi existen datos

app/models/concierto.rb
@@ -6,15 +6,15 @@ class Concierto < ApplicationRecord
   default_scope { order(:fecha, :hora) }
 
   def fecha_display
-    I18n.l(fecha, format: :day_name_day_number_month_name)
+    fecha_display ? I18n.l(fecha, format: :day_name_day_number_month_name) : ''
   end
 
   def fecha_corta
-    fecha.strftime('%d/%m/%y')
+    fecha ? fecha.strftime('%d/%m/%y') : ''
   end
 
   def hora_display
-    I18n.l(hora, format: :short)
+    hora ? I18n.l(hora, format: :short) : ''
   end
 
   def nombre_compositor

guillermobp/edmchc

app/models/concierto.rb
@@ -6,15 +6,15 @@ class Concierto < ApplicationRecord
   default_scope { order(:fecha, :hora) }
 
   def fecha_display
-    I18n.l(fecha, format: :day_name_day_number_month_name)
+    fecha ? I18n.l(fecha, format: :day_name_day_number_month_name) : ''
   end
 
   def fecha_corta
-    fecha.strftime('%d/%m/%y')
+    fecha ? fecha.strftime('%d/%m/%y') : ''
   end
 
   def hora_display
-    I18n.l(hora, format: :short)
+    hora ? I18n.l(hora, format: :short) : ''
   end
 
   def nombre_compositor