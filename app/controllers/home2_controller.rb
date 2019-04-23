fcd66d2cb75a9c53adf440695cda8fd97b67974c
Guillermo Barriga Placencia <guillermo@bitbox.cl>
2019-04-23 07:19:02 -0400

    :alembic: muestra imágenes aleatorias entre secciones de home alternativo

--- app/controllers/home2_controller.rb
+++ app/controllers/home2_controller.rb
@@ -1,5 +1,8 @@
 class Home2Controller < ApplicationController
   def index
     @encuentro = Encuentro.where(habilitado: true).last
+
+    # TODO: Sacar al modelo, si se acepta esta propuesta.
+    @fotos_aleatorias = (@encuentro.fotos + @encuentro.fotos_ensayos).sample(7)
   end
 end
