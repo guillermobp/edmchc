3490cb721ec5a1720198f5c0f7278a3018fbd22f
Guillermo Barriga Placencia <guillermo@bitbox.cl>
2019-04-26 07:08:53 -0400

    :alembic: muestra 3 videos y 8 fotos de forma aleatoria en home2

--- app/controllers/home2_controller.rb
+++ app/controllers/home2_controller.rb
@@ -4,5 +4,8 @@ class Home2Controller < ApplicationController
 
     # TODO: Sacar al modelo, si se acepta esta propuesta.
     @fotos_aleatorias = (@encuentro.fotos + @encuentro.fotos_ensayos).sample(7)
+    @conciertos_para_videos = @encuentro.conciertos.select { |c| c.tiene_video? }.sample(3)
+    @fotos_ensayos = @encuentro.fotos_ensayos.sample(8)
+    @fotos = @encuentro.fotos.sample(8)
   end
 end
