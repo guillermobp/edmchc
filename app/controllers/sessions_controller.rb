adec4a566dc41032b22aafbb4114c38317478623
Guillermo Barriga Placencia <guillermo@bitbox.cl>
2019-07-04 11:34:12 -0400

    avances...

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      remember user
       redirect_to encuentros_path
     else
       flash[:alert] = 'Las credenciales ingresadas no coinciden; inténtelo nuevamente'
@@ -16,7 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to login_path
   end
 end
